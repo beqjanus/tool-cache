d08d8a4dd347d5d1f8715783b1e444027f414857
Beq Janus <beqjanus@gmail.com>
2022-12-21 02:55:24 +0000

    revert the change that breaks Mac builds

--- scripts/install/Unix-like.sh
+++ scripts/install/Unix-like.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
 echo ::group::Installing $folder_name to tool cache
-mv -T $RUNNER_TEMP/$folder_name $RUNNER_TOOL_CACHE/$folder_name
+mkdir -p $RUNNER_TOOL_CACHE/$folder_name/
+mv $RUNNER_TEMP/$folder_name/* $RUNNER_TOOL_CACHE/$folder_name/
 echo ::endgroup::
